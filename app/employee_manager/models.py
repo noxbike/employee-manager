bc94653125e4e17cdd4913a945d8c4dbfdc683d0
noxbike <noxbike@gmail.com>
2025-12-29 21:51:34 +0400

    create my first model employee and add volume on docker compose

--- app/employee_manager/models.py
+++ app/employee_manager/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True)
+    position = models.DecimalField(max_digits=10, decimal_places=2)
+    hire_date = models.DateField()
+
+    def __str__(self):
+        return self.first_name
