c87ca7a5d31a1bb8c4d2f10ef620ec23289ce080
noxbike <noxbike@gmail.com>
2026-01-01 20:55:44 +0400

    add template liste employee and add salary field in employee models

--- app/employee_manager/models.py
+++ app/employee_manager/models.py
@@ -5,6 +5,7 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
     position = models.DecimalField(max_digits=10, decimal_places=2)
+    salary = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     hire_date = models.DateField()
 
     def __str__(self):
--- app/employee_manager/views.py
+++ app/employee_manager/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Employee
 
 # Create your views here.
+
+def employee_list(request):
+    remployes = Employee.objects.all()
+    employes = [{'name': 'mickael', 'email': 'noxbike@gmail.com', 'position': 'developer', 'salary': 50000},]
+    return render(request , 'employe/list.html', {'employes': employes})
+
+
