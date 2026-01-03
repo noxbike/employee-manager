7fc1530c2dcd7a126aaf8fcfbd765d26b052104b
noxbike <noxbike@gmail.com>
2026-01-03 10:56:15 +0400

    add edit, and delete function with a page confirm_delete

--- app/employee_manager/foms.py
+++ app/employee_manager/foms.py
@@ -6,5 +6,10 @@ class EmployeForm(forms.ModelForm):
         model = Employee
         fields = ['first_name', 'last_name', 'email', 'position', 'salary', 'hire_date']
         widgets = {
-            'hire_date': forms.DateInput(attrs={'type': 'date'}),
+            'first_name': forms.TextInput(attrs={'class': 'input w-full', 'placeholder': 'First Name'}),
+            'last_name': forms.TextInput(attrs={'class': 'input w-full', 'placeholder': 'Last Name'}),
+            'email': forms.EmailInput(attrs={'class': 'input w-full', 'placeholder': 'Email'}),
+            'position': forms.TextInput(attrs={'class': 'input w-full', 'placeholder': 'Position'}),
+            'salary': forms.NumberInput(attrs={'class': 'input w-full', 'placeholder': '3000.00'}),
+            'hire_date': forms.DateInput(attrs={'class': 'input w-full', 'type': 'date'}),
         }
--- app/employee_manager/views.py
+++ app/employee_manager/views.py
@@ -14,3 +14,18 @@ def add_employee(request):
         form.save()
         return redirect('employee_list')
     return render(request, 'employe/formulaire.html', {'form': form})
+
+def edit_employee(request, employee_id):
+    employe = Employee.objects.get(id=employee_id)
+    form = EmployeForm(request.POST or None, instance=employe)
+    if form.is_valid():
+        form.save()
+        return redirect('employee_list')
+    return render(request, 'employe/formulaire.html', {'form': form})
+
+def delete_employee(request, employee_id):
+    employe = Employee.objects.get(id=employee_id)
+    if request.method == 'POST':
+        employe.delete()
+        return redirect('employee_list')
+    return render(request, 'employe/confirm_delete.html', {'employe': employe})
